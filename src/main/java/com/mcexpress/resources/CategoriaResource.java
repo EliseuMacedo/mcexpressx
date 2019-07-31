13d2f3a84f562e10959c2a5e2301d749f9fa4d75
EliseuMacedo <eliseumcd@gmail.com>
2019-07-31 14:29:14 -0300

    Categoria BackEnd implementada

--- src/main/java/com/mcexpress/resources/CategoriaResource.java
+++ src/main/java/com/mcexpress/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.mcexpress.resources; // o nome Resources é utilizado para criar a classe de controlador rest
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mcexpress.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method = RequestMethod.GET) //verbo http para dados
-	public String listar() {
-		return "Rest funcionando";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
