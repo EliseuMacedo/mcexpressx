453ce97dfb744a2fa5d023857d56da3454a40c65
EliseuMacedo <eliseumcd@gmail.com>
2019-07-31 11:07:41 -0300

    Start a project

---
+++ src/main/java/com/mcexpress/McexpressxApplication.java
@@ -0,0 +1,13 @@
+package com.mcexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McexpressxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McexpressxApplication.class, args);
+	}
+
+}
